3d6ca53182d6a6a19fc48b51f8601afd0f4c1060
shubhamkumarsingh <www.shubhamsingh6666@gmail.com>
2022-08-03 15:45:59 +0530

    first Commit

---
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.activitylifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast toast=Toast.makeText(this,"onCreate",Toast.LENGTH_LONG);
+        toast.show();
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Toast toast=Toast.makeText(this,"onStart",Toast.LENGTH_LONG);
+        toast.show();
+    }
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Toast toast=Toast.makeText(this,"onRestart",Toast.LENGTH_LONG);
+        toast.show();
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Toast toast=Toast.makeText(this,"onResume",Toast.LENGTH_LONG);
+        toast.show();
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Toast toast=Toast.makeText(this,"onPause",Toast.LENGTH_LONG);
+        toast.show();
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Toast toast=Toast.makeText(this,"onStop",Toast.LENGTH_LONG);
+        toast.show();
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Toast toast=Toast.makeText(this,"onDestroy",Toast.LENGTH_LONG);
+        toast.show();
+    }
+}
